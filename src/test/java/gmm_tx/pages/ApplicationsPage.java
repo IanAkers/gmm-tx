b4f2a45f21effb5898366971ff76715fc0e8597a
Justin Giron <giron_justin@bah.com>
2017-09-12 10:18:21 -0400

    doing this commit for help w the problem finding ApplicationsLanding page 'create-application' HTML ATTR ID from bp (201709120844L EL MARTES GIRO @ BAH NBP304)

--- src/test/java/gmm_tx/pages/ApplicationsPage.java
+++ src/test/java/gmm_tx/pages/ApplicationsPage.java
@@ -11,19 +11,21 @@ import net.thucydides.core.pages.PageObject;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 // there's a better way to do this that leaves out http://ui-dev.ocp-apps.bahincubator.com
 
 //@DefaultUrl("http://ui-dev.ocp-apps.bahincubator.com/Application/applicationsLanding")
 @DefaultUrl("http://localhost:8080/Application/applicationsLanding")
 
+								//	@FindBy(id = "create-application")
 public class ApplicationsPage extends PageObject {
 	private WebDriver webDriver;
-	@FindBy(id = "create-application")
-	WebElementFacade createApplication_Link;
+	@FindBy(xpath = "//a[@id=‘create-application’]")
+	WebElementFacade createApplicationLink;
 
-	public ApplicationsPage(WebDriver driver) {
-		super(driver);
-		webDriver = driver;
+	public ApplicationsPage(WebDriver webDriver) {
+		super(webDriver);
+		webDriver = webDriver;
 		System.out.println("WE GOT TO THE CXR FOR ApplicationsPage");
 		if (webDriver == null) {
 			System.out.println("webDriver IS NULL!!");
@@ -32,9 +34,8 @@ public class ApplicationsPage extends PageObject {
 	}
 
 	public void createApplication() {
-
 		System.out.println("IN METHOD .. createApplication");
-		System.out.println("ABOUT TO CLICK createApplication_Link...!!");
-		createApplication_Link.click();
+		System.out.println("ABOUT TO CLICK createApplicationLink...!!");
+		createApplicationLink.click();
 	}
 }

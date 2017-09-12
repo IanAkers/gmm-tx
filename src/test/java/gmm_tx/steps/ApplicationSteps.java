3ecd23b8349dc256f1fd0e00bb129e42abcd342b
Prin, Brendan [USA] <prin_brendan@bah.com>
2017-09-12 11:08:37 -0400

    Update ApplicationSteps.java

--- src/test/java/gmm_tx/steps/ApplicationSteps.java
+++ src/test/java/gmm_tx/steps/ApplicationSteps.java
@@ -1,46 +1,18 @@
 package gmm_tx.steps;
 
-import net.thucydides.core.annotations.DefaultUrl;
-import net.thucydides.core.annotations.Steps;
-import net.thucydides.core.pages.PageObject;
-
-import java.io.File;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import gmm_tx.pages.ApplicationsPage;
 import gmm_tx.pages.HomePage;
-import gmm_tx.steps.serenity.EndUserSteps;
 
 public class ApplicationSteps {
 
-	WebDriver webDriver = null;
-
-	public ApplicationSteps() {
-
-	}
+	ApplicationsPage applicationsPage;
+	HomePage homePage;
 
 	@Given("^the user has logged into GMM$")
 	public void the_user_has_logged_into_GMM() throws Throwable {
-		System.out.println("the_user_has_logged_into_GMM ... NOT IN THE CXR...");
-		File drvFile = new File("/Applications/gg/chromedriver");
-		if (drvFile.exists()) {
-			if (drvFile.canExecute()) {
-				System.out.println("Found chrome driver and can execute it");
-			}
-		} else {
-			System.out.println("Can't find chrome driver");
-		}
-		System.out.println("\t about to call webDriver.get ..  IN FILE ApplicationSteps.java");
-		System.setProperty("webdriver.chrome.driver", "/Applications/gg/chromedriver");
-		webDriver = new ChromeDriver();
-		HomePage homePage = new HomePage(webDriver);
 		homePage.openHomePage();
 		String title = homePage.getTitle();
 		System.out.println("\t\t@GIVEN:	\t THIS PAGE TITLE IS: " + title);
@@ -53,49 +25,30 @@ public class ApplicationSteps {
 				"\t@WHEN:	\t the_user_clicks_on_the_sidebar_link_for_application ..  IN FILE ApplicationSteps.java");
 		System.out.println("CLICKS THE clickApplication LINK");
 
-		WebElement clickAppLink = webDriver.findElement(By.id("clickApplication"));
+		homePage.userClicksOnSidebarLinkForAPPLICATION();
 		System.out.println("FOUND THE clickAppLink HTML ATTR Id");
-
-		if (clickAppLink.isEnabled()) {
-			System.out.println("----clickAppLink LINK IS ENABLED ...");
-			clickAppLink.click();
-		} else {
-			System.out.println("----clickAppLink LINK IS not ENABLED -- BOO!!!");
-		}
 	}
 
 	@Then("^they should see the page that lists their application$")
 	public void they_should_see_the_page_that_lists_their_application() throws Throwable {
-		System.out.println(
-				"\t\t@THEN:	\t they_should_see_the_page_that_lists_their_application ..  IN FILE ApplicationSteps.java");
+		System.out.println("\t\t@THEN:	\t they_should_see_the_page_that_lists_their_application ..  IN FILE ApplicationSteps.java");
 	}
 
 	@Given("^the user is on the page that shows applications$")
 	public void the_user_is_on_the_page_that_shows_applications() throws Throwable {
-		System.out.println(
-				"@GIVEN:	\t the_user_is_on_the_page_that_shows_applications ..  IN FILE ApplicationSteps.java");
-		System.setProperty("webdriver.chrome.driver", "/Applications/gg/chromedriver");
-		webDriver = new ChromeDriver();
-		if (webDriver == null) {
-			System.out.println("IN ApplicationSteps.the_user_is_on_the_page_that_shows...;  webDriver is NULL");
-		}
-		ApplicationsPage applicationsPage = new ApplicationsPage(webDriver);
 		applicationsPage.createApplication();
-		System.out.println(
-				"\t\t@GIVEN .. CALL METHOD .. applicationsPage.createApplication(); IN FILE ApplicationSteps.java");
+		System.out.println("\t\t@GIVEN .. CALL METHOD .. applicationsPage.createApplication(); IN FILE ApplicationSteps.java");
 	}
 
 	@When("^the user clicks on the link to create application$")
 	public void the_user_clicks_on_the_link_to_create_application() throws Throwable {
-		System.out.println(
-				"\t@WHEN:	\t the_user_clicks_on_the_link_to_create_application ..  IN FILE ApplicationSteps.java");
+		System.out.println("\t@WHEN:	\t the_user_clicks_on_the_link_to_create_application ..  IN FILE ApplicationSteps.java");
 		System.out.println("JUST CLICKED: ... createApplicationLink");
 	}
 
 	@Then("^they should see the page to enter application data$")
 	public void they_should_see_the_page_to_enter_application_data() throws Throwable {
-		System.out.println(
-				"\t\t@THEN:	\t they_should_see_the_page_to_enter_application_data ..  IN FILE ApplicationSteps.java");
+		System.out.println("\t\t@THEN:	\t they_should_see_the_page_to_enter_application_data ..  IN FILE ApplicationSteps.java");
 	}
 
 	@Given("^the user has entered all application data$")

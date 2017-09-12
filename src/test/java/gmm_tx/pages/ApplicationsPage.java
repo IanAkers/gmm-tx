0b001c7209ed1cac502054417ae6758622dd19a5
Prin, Brendan [USA] <prin_brendan@bah.com>
2017-09-12 11:07:54 -0400

    Update ApplicationsPage.java
    Refactored your ApplicationsPage

--- src/test/java/gmm_tx/pages/ApplicationsPage.java
+++ src/test/java/gmm_tx/pages/ApplicationsPage.java
@@ -1,41 +1,23 @@
 package gmm_tx.pages;
 
-import net.thucydides.core.annotations.DefaultUrl;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WebDriver;
-import net.serenitybdd.core.pages.WebElementFacade;
-import java.util.stream.Collectors;
 import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 // there's a better way to do this that leaves out http://ui-dev.ocp-apps.bahincubator.com
-
 //@DefaultUrl("http://ui-dev.ocp-apps.bahincubator.com/Application/applicationsLanding")
 @DefaultUrl("http://localhost:8080/Application/applicationsLanding")
-
-								//	@FindBy(id = "create-application")
 public class ApplicationsPage extends PageObject {
-	private WebDriver webDriver;
+
+	//@FindBy(id = "create-application")
 	@FindBy(xpath = "//a[@id=‘create-application’]")
 	WebElementFacade createApplicationLink;
-
-	public ApplicationsPage(WebDriver webDriver) {
-		super(webDriver);
-		webDriver = webDriver;
-		System.out.println("WE GOT TO THE CXR FOR ApplicationsPage");
-		if (webDriver == null) {
-			System.out.println("webDriver IS NULL!!");
-		}
-
-	}
 
 	public void createApplication() {
 		System.out.println("IN METHOD .. createApplication");
 		System.out.println("ABOUT TO CLICK createApplicationLink...!!");
 		createApplicationLink.click();
 	}
+	
 }
